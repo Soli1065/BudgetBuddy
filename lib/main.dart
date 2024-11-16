a442b2e76ef952bee00fdd19e6231aea46fae262
Soli <s.hosseini.309@gmail.com>
2024-11-16 14:15:55 -0500

    #init

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:budget_buddy/src/features/home/home_page.dart';
+import 'package:budget_buddy/src/features/home/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const HomeScreen(),
     );
   }
 }
